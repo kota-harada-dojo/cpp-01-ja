2d922b2ad83968f8494e2f3542fb8f0eccc28470
kota-harada <kota.harada@woven-planet.global>
2023-12-04 23:54:11 +0900

    challenge_01

---
+++ 01_introduction_to_cpp/01_challenge/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+int main() {
+    int myNumber = 10;
+    int anotherNumber = 5;
+    int answer = myNumber + anotherNumber;
+
+    std::cout << answer << std::endl;
+    return 0;
+}
+
