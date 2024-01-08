5899d2376c1434991375bee35ad1c1eeab273bf4
kota-harada <kota.harada@woven-planet.global>
2024-01-08 13:42:02 +0900

    save

---
+++ 04_memory_and_error_handling/03_challenge/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+// コードを入力してください
+
+std::vector<int> double_elements(std::vector<int> target) {
+  std::vector<int> result;
+  for (int i = 0; i < target.size(); i++) {
+    result.push_back(target[i] * 2);
+  }
+
+  return result;
+}
+
+// void double_elements(std::vector<int>& target) {
+//   for (int i = 0; i < target.size(); i++) {
+//     target[i] = target[i] * 2;
+//   }
+
+//   return;
+// }
+
+int main() {
+  std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+  std::vector<int> result = double_elements(numbers);
+
+  // コードを入力してください
+  for (int i = 0; i < numbers.size(); i++) {
+    std::cout << "Result of double_elements: " << result[i] << std::endl;
+    std::cout << "Result of origin: " << numbers[i] << std::endl;
+  }
+
+  return 0;
+}
